ea7f5d8b7734925b6c0ce0a93bfa372d4f34cac6
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-03 18:41:48 -0600

    Updated

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const mysql2 = require('mysql2');
 const cTable = require('console.table');
-//const inquirer = require('inquirer');
-//import mysql2 from 'mysql2'
+const inquirer = require('inquirer');
+// import mysql2 from 'mysql2'
 
 
 const connection = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'employees_db'
+});
+connection.connect(function() {
+    console.log('succefully connected to a database')
 });
