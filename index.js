c3e4564d85f753fd02fb1acb040664ce12188e07
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-05 17:10:57 -0600

    working out bug

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+//Imports mysql2, console.table, inquirer
 const mysql2 = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 // import mysql2 from 'mysql2'
-
-
+//Connection to database
 const connection = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'employees_db'
 });
-connection.connect(function() {
+connection.connect(function(err) {
+    if (err) throw err;
     console.log('succefully connected to a database')
 });
